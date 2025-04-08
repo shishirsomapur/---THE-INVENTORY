012d5dc7d57bf3a21197beac44ce41e44a319cc5
shishirsomapur <shishir6364@gmail.com>
2025-04-08 15:11:07 +0530

    updated db config

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'crossover.proxy.rlwy.net',
     user: 'root',
-    password: 'admin123',
-    database: 'stocks',
+    password: 'ZnisjrNIbFWOzqKrjXGqaShUuxPuJIZb',
+    database: 'railway',
+    port: 21796,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
